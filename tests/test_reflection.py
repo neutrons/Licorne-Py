b0a34ab4a6b2f5ffd080283b861302810849ebab
Steven Hahn <hahnse@ornl.gov>
2017-09-29 16:26:33 -0400

    more pythonic resolution function.

--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -57,59 +57,41 @@ class TestReflectionClass(unittest.TestCase):
                 reference_values = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/res_mode'+str(res_mode)+'refl'+str(k+1)+'.dat'), unpack=True)
                 assert_array_almost_equal(reference_values, RRr)
 
-class resolution:
-    def __init__(self):
-        self.Theta1 = 0.0068
-        self.Theta2 = 0.01
-        self.Theta3 = 0.017
-        self.DTheta1 = 0.0003
-        self.DTheta2 = 0.0005
-        self.DTheta3 = 0.0009
-        self.Q1 = 0.03
-        self.Q2 = 0.045
-        self.DLambda = 0.005
-    
-    def LambdaQP1(self, Q):
-        return 4.0 * np.pi * np.sin(self.Theta1) / Q
-    def LambdaQP2(self, Q):
-        return 4.0 * np.pi * np.sin(self.Theta2) / Q
-    def LambdaQP3(self, Q):
-        return 4.0 * np.pi * np.sin(self.Theta3) / Q
-    def Lambda(self, Q):
-        if Q < self.Q1:
-            return self.LambdaQP1(Q)
-        elif Q < self.Q2:
-            return self.LambdaQP2(Q)
-        else:
-            return self.LambdaQP3(Q)
+def res_chi3_137(Q):
+    Theta1 = 0.0068
+    Theta2 = 0.01
+    Theta3 = 0.017
 
-    def SigmaQP1(self, Q):
-        return Q * np.sqrt(np.power(self.DTheta1 / self.Theta1, 2) + np.power(self.DLambda / self.LambdaQP1(Q), 2))
+    DTheta1 = 0.0003
+    DTheta2 = 0.0005
+    DTheta3 = 0.0009
 
-    def SigmaQP2(self, Q):
-        return Q * np.sqrt(np.power(self.DTheta2 / self.Theta2, 2) + np.power(self.DLambda / self.LambdaQP2(Q), 2))
+    Q1 = 0.03
+    Q2 = 0.045
 
-    def SigmaQP3(self, Q):
-        return Q * np.sqrt(np.power(self.DTheta3 / self.Theta3, 2) + np.power(self.DLambda / self.LambdaQP3(Q), 2))
+    QP1 = Q < Q1
+    QP2 = np.logical_and(Q >= Q1,Q <= Q2)
+    QP3 = Q > Q2
 
-    def Sigma(self, Q):
-        if(Q < self.Q1):
-            return self.SigmaQP1(Q)
-        elif (Q < self.Q2):
-            return self.SigmaQP2(Q)
-        else:
-            return self.SigmaQP3(Q)
+    DLambda = 0.005
+    Lambda = np.zeros(len(Q))
+    sigma = np.zeros(len(Q))
+
+    Lambda[QP1] = 4.0 * np.pi * np.sin(Theta1) / Q[QP1]
+    Lambda[QP2] = 4.0 * np.pi * np.sin(Theta2) / Q[QP2]
+    Lambda[QP3] = 4.0 * np.pi * np.sin(Theta3) / Q[QP3]
+
+    sigma[QP1] = Q[QP1] * np.sqrt((DTheta1/Theta1)**2+(DLambda/Lambda[QP1])**2)
+    sigma[QP2] = Q[QP2] * np.sqrt((DTheta2/Theta2)**2+(DLambda/Lambda[QP2])**2)
+    sigma[QP3] = Q[QP3] * np.sqrt((DTheta3/Theta3)**2+(DLambda/Lambda[QP3])**2)
+
+    return sigma
 
 class Testchi3_137(unittest.TestCase):
-    q = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/chi3_137/q.dat'),unpack=True)
-    inc_moment = q / 2.0
+    Q = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/chi3_137/q.dat'),unpack=True)
+    inc_moment = Q / 2.0
 
-    res = resolution()
-    sigma = []
-    for value in q:
-        sigma.append(res.Sigma(value))
-
-    sigma = np.array(sigma)
+    sigma = res_chi3_137(Q)
 
     substrate = complex(3.6214e-006, 0.0)
     layer_info = np.loadtxt(os.path.join(os.path.dirname(__file__), 'data/chi3_137/profile_sublayers.dat'))
@@ -130,38 +112,34 @@ class Testchi3_137(unittest.TestCase):
     norm_factor=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
     background=1.2e-05;
 
-    pol_eff = np.ones(len(q), dtype = np.complex128)
-    an_eff = np.ones(len(q), dtype = np.complex128)
+    pol_eff = np.ones(len(Q), dtype = np.complex128)
+    an_eff = np.ones(len(Q), dtype = np.complex128)
 
     n_of_outputs = 3
     for k in range(n_of_outputs):
         RR = reflection.spin_av(R, pol_vecs[k], an_vecs[k], pol_eff, an_eff)
         RR = np.real(RR)
-        RRr = reflection.resolut(RR, q, sigma, 3)
+        RRr = reflection.resolut(RR, Q, sigma, 3)
         RRr = RRr * norm_factor[k] + background
         fig,ax = plt.subplots()
-        ax.semilogy(q,RRr,label='calculation')
+        ax.semilogy(Q,RRr,label='calculation')
         #plt.xlim([0.0,0.06])
         #plt.ylim([1.0e-4,10.0])
         reference_values = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/chi3_137/rtheory'+str(k+1)+'.dat'), unpack=True)
         assert_array_almost_equal(RRr,reference_values,2)
-        ax.semilogy(q, reference_values, label='reference')
+        ax.semilogy(Q, reference_values, label='reference')
         ax.set_xlabel('Momentum Transfer $\AA^{-1}$')
         ax.set_ylabel('Reflectivity')
         ax.set_title('Polarization ({},{},{}), Analysis({},{},{})'.format(pol_vecs[k][0],pol_vecs[k][1],pol_vecs[k][2],an_vecs[k][0],an_vecs[k][1],an_vecs[k][2]))
         ax.legend()
         fig.savefig('chi3_137_'+str(k+1)+'.pdf')
-        plt.close()        
+        plt.close()
 
 class Testr2_6_508(unittest.TestCase):
     q = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/r2_6_508/q.dat'),unpack=True)
     inc_moment = q / 2.0
 
-    res = resolution()
-    sigma = []
-    for value in q:
-        sigma.append(res.Sigma(value))
-    sigma = np.array(sigma)
+    sigma = res_chi3_137(q)
 
     substrate = complex(3.533e-006, 0.0)
     layer_info = np.loadtxt(os.path.join(os.path.dirname(__file__), 'data/r2_6_508/profile_sublayers.dat'))
@@ -203,37 +181,24 @@ class Testr2_6_508(unittest.TestCase):
         fig.savefig('r2_6_508_'+str(k+1)+'.pdf')
         plt.close()
 
-class resolution2:
-    def __init__(self):
-        self.DTheta1 = 0.0
-        self.DTheta2 = 0.0007
-        self.Q1 = 0.0
-        self.Lambda = 5.0
-        self.DLambda = 0.01
+def res_helix100(Q):
+    DTheta1 = 0.0
+    DTheta2 = 0.0007
+    Q1 = 0.0
+    Lambda = 5.0
+    DLambda = 0.01
 
-    def SigmaQP1(self, Q):
-        Theta = np.arcsin(Q*self.Lambda/(4.0*np.pi));
-        return Q * np.sqrt(np.power(self.DTheta1 / Theta, 2) + np.power(self.DLambda / self.Lambda, 2))
-
-    def SigmaQP2(self, Q):
-        Theta = np.arcsin(Q*self.Lambda/(4.0*np.pi));
-        return Q * np.sqrt(np.power(self.DTheta2 / Theta, 2) + np.power(self.DLambda / self.Lambda, 2))
-
-    def Sigma(self, Q):
-        if(Q < self.Q1):
-            return self.SigmaQP1(Q)
-        else:
-            return self.SigmaQP2(Q)
+    Theta = np.arcsin(Q * Lambda / (4.0 * np.pi))
+    DTheta = np.zeros(len(Q));
+    DTheta[Q > Q1] = 0.0007;
+    Sigma = Q * np.sqrt((DTheta / Theta)**2 + (DLambda / Lambda)**2);
+    return Sigma
 
 class Testhelix100(unittest.TestCase):
     q = np.loadtxt(os.path.join(os.path.dirname(__file__),'data/helix100/q.dat'),unpack=True)
     inc_moment = q / 2.0
 
-    res = resolution2()
-    sigma = []
-    for value in q:
-        sigma.append(res.Sigma(value))
-    sigma = np.array(sigma)
+    sigma = res_helix100(q)
 
     substrate = complex(6.0e-6, 0.0)
     layer_info = np.loadtxt(os.path.join(os.path.dirname(__file__), 'data/helix100/profile_sublayers.dat'))
