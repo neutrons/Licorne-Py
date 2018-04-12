8158283665d46a7f555867c0806e561877001348
Mathieu Doucet <doucetm@ornl.gov>
2018-04-12 11:36:22 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
-import sys
 import os
 from setuptools import setup
 package_data = {"licorne": ["UI/*.ui",]}
 
 _scripts = ["bin/Licorne"]
-if "bdist_rpm" in sys.argv and os.path.isdir("/SNS/software/miniconda2/envs/licorne"):
+if os.path.isdir("/SNS/software/miniconda2/envs/licorne"):
     _scripts =["bin/licorne"]
 
 setup(
     name='licorne-py',
-    version='0.1.0',
+    version='0.1.2',
     description='',
     author='Steven Hahn, Andrei Savici',
     author_email='hahnse@ornl.gov, saviciat@ornl.gov',
